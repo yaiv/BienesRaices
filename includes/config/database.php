d6450732554d92c485eefa9b98a1fb1bfdba7af9
yaiv <yaiv.gm@gmail.com>
2025-06-03 11:47:54 -0600

    Se actualiza BD y se adaptara arquitectura MVC

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 
 //conexion con BD
 function conectarDB() : mysqli {
-    $db = new mysqli('localhost', 'root', 'root', 'bienesraices_crud');
+    $db = new mysqli('localhost', 'root', 'root', 'bienesraices');
 
     if(!$db){
         echo "Error no se pudo conectar";
